bb6a95c1be57b2fccd2d7b42a616067a4e3509e4
Syrovatskiy Znenia <Zadolbalyse@yandex.ru>
2024-02-12 20:47:30 +0700

    create main with button fixed

--- frontend/src/components/Main/Inform.tsx
+++ frontend/src/components/Main/Inform.tsx
@@ -1,31 +1,32 @@
 import styles from './Inform.module.css';
-import Restangle from '../../assets/Rectangle .png';
+import Rectangle from '../../assets/Rectangle .png';
 import Container from '../Container/Container.tsx';
 import PC from '../../assets/PC.png';
-import PCBtn from '../PCBtn/PCBtn.tsx';
+import Btn from '../PCBtn/PCBtn.tsx';
 
 function Inform() {
   return (
-    <body className={styles.body}>
+    <section className={styles.basis}>
       <Container className={styles.container}>
-        <img src={Restangle} alt={'Restangle'} className={styles.Restangle} />
+        <img src={Rectangle} alt={'Rectangle'} className={styles.Rectangle} />
         <img src={PC} alt={'PC'} className={styles.PC} />
         <div className={styles.mainText}>
           <h1 className={styles.box}> Компьютерная коробка </h1>
           <p className={styles.paragraph}>
             Вместе с нами вы сможете проверить совместимость комплектующих для
-            вашего <br /> PC-зверя или выбрать уже готовую сборку, которую уже
-            за вас собрали другие пользователи и проверили временем, <br />
-            именно поэтому скорее переходи к сборке возможно своего первого
-            компьютера мечты
+            вашего PC-зверя или выбрать уже готовую сборку, которую уже за вас
+            собрали другие пользователи и проверили временем, именно поэтому
+            скорее переходи к сборке возможно своего первого компьютера мечты
           </p>
           <span className={styles.quote}>
             «Соберите PC своей мечты вместе с нами»
           </span>
-          <PCBtn>собрать пк</PCBtn>
+          <div className={styles.button}>
+            <Btn>собрать пк</Btn>
+          </div>
         </div>
       </Container>
-    </body>
+    </section>
   );
 }
 
