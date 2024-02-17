5456f8678c6966043caa6b0e70d8fa886df1ac73
konyasharin <kolechka.malyshev.04@gmail.com>
2024-02-17 20:17:49 +0700

    scale component finished

--- frontend/src/pages/PCBuildPage/PCBuildPage.tsx
+++ frontend/src/pages/PCBuildPage/PCBuildPage.tsx
@@ -1,15 +1,26 @@
 import Container from '../../components/Container/Container.tsx';
 import styles from './PCBuildPage.module.css';
+import { useState } from 'react';
 
 function PCBuildPage() {
-  const scaleInner = 50;
-
+  const [scaleInnerPercents] = useState(30);
+  let colorScaleInner = '';
+  if (scaleInnerPercents <= 50) {
+    colorScaleInner = '#FF0B0B';
+  } else if (scaleInnerPercents > 50 && scaleInnerPercents <= 80) {
+    colorScaleInner = '#F7A400';
+  } else if (scaleInnerPercents > 80) {
+    colorScaleInner = '#16D840';
+  }
   return (
     <Container>
       <div className={styles.scale}>
         <div
           className={styles.scaleInner}
-          style={{ width: `calc(${scaleInner}% + 6px)` }}
+          style={{
+            width: `calc(${scaleInnerPercents}% + 6px)`,
+            backgroundColor: `${colorScaleInner}`,
+          }}
         ></div>
       </div>
     </Container>
