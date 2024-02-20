fabd8e143eb25a314ca980c1ba5ac3f678d7aecf
konyasharin <kolechka.malyshev.04@gmail.com>
2024-02-21 01:02:50 +0700

    migrate to c#

--- backend/src/main/java/com/example/pc/repositories/SSDRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.pc.repositories;
-
-import com.example.pc.Models.SSDModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SSDRepo extends JpaRepository<SSDModel, Long> {
-}
