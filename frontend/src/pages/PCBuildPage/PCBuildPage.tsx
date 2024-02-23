1fd930ed890245681034befa91d91e7ed3beb04c
Syrovatskiy Znenia <Zadolbalyse@yandex.ru>
2024-02-23 19:14:23 +0700

    PowerOfBuild component fix

--- frontend/src/pages/PCBuildPage/PCBuildPage.tsx
+++ frontend/src/pages/PCBuildPage/PCBuildPage.tsx
@@ -1,11 +1,15 @@
 import Container from '../../components/Container/Container.tsx';
 import Scale from './Scale/Scale.tsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BuildsErrors from './BuildsErrors/BuildsErrors.tsx';
 import PowerOfBuild from './PowerOfBuild/PowerOfBuild.tsx';
 
 function PCBuildPage() {
   const [scalePercents] = useState(40);
+  const [power, setPower] = useState<number>(0);
+  useEffect(() => {
+    setPower(5);
+  }, []);
   return (
     <Container>
       <Scale percents={scalePercents} />
@@ -19,7 +23,7 @@ function PCBuildPage() {
         title={'Процессор'}
         description={'Выберите процессор из списка'}
       />
-      <PowerOfBuild power={10} />
+      <PowerOfBuild power={power} price={'100$'} />
     </Container>
   );
 }
--- frontend/src/pages/PCBuildPage/PowerOfBuild/PowerOfBuild.tsx
+++ frontend/src/pages/PCBuildPage/PowerOfBuild/PowerOfBuild.tsx
@@ -1,25 +1,32 @@
 import styles from './PowerOfBuild.module.css';
 import greyQuestion from '../../../assets/greyQuestion.png';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 enum color {
   white = '#FFFFFF',
 }
 
-const PowerOfBuild: React.FC<{ power: number }> = props => {
-  const [circleOpacity, setCircleOpacity] = useState<number[]>([]);
-  const price = '';
-  useEffect(() => {
-    const opacityArray: number[] = [];
-    for (let i = 1; i <= 10; i++) {
-      if (i <= props.power) {
-        opacityArray.push(0.1 * i);
-      } else {
-        opacityArray.push(i);
-      }
+const PowerOfBuild: React.FC<{ power: number; price: string }> = props => {
+  const opacityArray: number[] = [];
+  for (let i = 1; i <= 10; i++) {
+    if (i <= props.power) {
+      opacityArray.push(0.1 * i);
+    } else {
+      opacityArray.push(0);
     }
-    setCircleOpacity(opacityArray);
-  }, [props.power]);
+  }
+  const circlesArray = opacityArray.map((circle, i) => {
+    return (
+      <div
+        key={i}
+        className={styles.circle}
+        style={{
+          backgroundColor:
+            i < props.power ? `rgba(0, 0, 255, ${circle})` : color.white,
+        }}
+      ></div>
+    );
+  });
   return (
     <div>
       <div className={styles.powerBuild}>
@@ -28,24 +35,11 @@ const PowerOfBuild: React.FC<{ power: number }> = props => {
         <div className={styles.greyCircle}>
           <img src={greyQuestion} alt={'greyQuestion'} />
         </div>
-        <div className={styles.powerCircles}>
-          {Array.from({ length: 10 }).map((_, index) => (
-            <div
-              key={index}
-              className={styles.circle}
-              style={{
-                backgroundColor:
-                  index < props.power
-                    ? `rgba(0, 0, 255, ${circleOpacity[index]})`
-                    : color.white,
-              }}
-            ></div>
-          ))}
-        </div>
+        <div className={styles.powerCircles}>{...circlesArray}</div>
       </div>
       <div className={styles.priceBuild}>
         <h3>Итоговая цена:</h3>
-        <h3 className={styles.price}>{price}</h3>
+        <h3 className={styles.price}>{props.price}</h3>
       </div>
     </div>
   );
