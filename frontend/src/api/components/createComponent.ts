20d951a876de95c0cc773123cd1456c2a34483eb
konyasharin <kolechka.malyshev.04@gmail.com>
2024-03-04 01:16:18 +0700

    update create components

--- frontend/src/api/components/createComponent.ts
+++ frontend/src/api/components/createComponent.ts
@@ -1,8 +1,13 @@
 import { AxiosResponse } from 'axios';
+import axios from 'api/axios.ts';
 
-function createComponent<T>(
+async function createComponent<T>(
   url: string,
   data: Omit<T, 'id'>,
-): Promise<AxiosResponse<T>> {}
+): Promise<AxiosResponse<T>> {
+  return await axios.post(url, data, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
+}
 
 export default createComponent;
