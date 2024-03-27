02fb3a2a12fa7c1cd5f1e5385433f6a69d5df870
konyasharin <kolechka.malyshev.04@gmail.com>
2024-03-27 19:39:06 +0700

    completed components methods

--- backend/backend/Controllers/ComponentController.cs
+++ backend/backend/Controllers/ComponentController.cs
@@ -1,9 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System.Globalization;
 using System.Reflection;
 using backend.Entities;
-using backend.Entities.CommentEntities;
-using backend.Entities.ComponentsInfo;
-using backend.Entities.User;
 using backend.Utils;
 using Dapper;
 using Microsoft.AspNetCore.Mvc;
@@ -78,7 +75,7 @@ namespace backend.Controllers
         }
 
         protected async Task<IActionResult> GetAllComponents<T>(int limit, int offset, string databaseName,
-            string[] productCharacteristics) where T : Component<string>
+            string[] componentCharacteristics)
         {
             string[] characteristicsBase = ["product_id AS productId", "brand", "model", "country", "price", "description", "image", "amount", "power", "likes", "product_type AS productType"];
             try
@@ -86,25 +83,175 @@ namespace backend.Controllers
                 await using var connection = new NpgsqlConnection(connectionString);
                 {
                     connection.Open();
-                    var computerCasesBase = 
+                    var componentsBase = 
                         connection.Query<Component<string>>($"SELECT {TransformCharacteristicsToString(characteristicsBase)} " +
                                                             $"FROM public.products WHERE product_type = '{databaseName}' " +
                                                             $"LIMIT {limit} OFFSET {offset}");
-                    List<Dictionary<string, object>> computerCases = new List<Dictionary<string, object>>();
-                    foreach (var computerCase in computerCasesBase)
+                    List<Dictionary<string, object>> components = new List<Dictionary<string, object>>();
+                    foreach (var component in componentsBase)
                     {
-                        if (computerCase.ProductId != null)
+                        if (component.ProductId != null)
                         {
-                            computerCases.Add(await JoinProductInfo(computerCase.ProductId.Value, databaseName, computerCase, productCharacteristics));
+                            components.Add(await JoinComponentInfo<T>(component.ProductId.Value, databaseName, component, componentCharacteristics));
                         }
                     }
-                    return Ok(new { data = computerCases });
+                    return Ok(new { data = components });
                 }
             }
             catch (Exception ex)
             {
-                logger.LogError($"ComputerCase data did not get from database. Exception: {ex}");
+                logger.LogError($"Component data did not get from database. Exception: {ex}");
                 return NotFound(new { error = ex.Message });
+            }
+        }
+        
+        protected async Task<IActionResult> getComponent<T>(int componentId, string databaseName, string[] componentCharacteristics)
+        {
+            string[] characteristicsBase = ["product_id AS productId", "brand", "model", "country", "price", "description", "image", "amount", "power", "likes", "product_type AS productType"];
+            
+            try
+            {
+                await using var connection = new NpgsqlConnection(connectionString);
+                {
+                    connection.Open();
+                    
+                    var componentBase = connection.QueryFirstOrDefault<Component<string>>($"SELECT {TransformCharacteristicsToString(characteristicsBase)} FROM public.products " +
+                        $"WHERE product_id = {componentId}");
+                    
+                    if (componentBase != null)
+                    {
+                        var response = await JoinComponentInfo<T>(componentId, databaseName, componentBase, componentCharacteristics);
+                        logger.LogInformation($"Retrieved component with Id {componentId} from the database");
+                        return Ok(new { response });
+                    }
+                    else
+                    {
+                        logger.LogInformation($"Component with Id {componentId} not found in the database");
+                        return NotFound(new {error = $"Not found component with {componentId}"});
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Failed to retrieve component data from the database. \nException {ex}");
+                return StatusCode(500, new { error = ex.Message });
+            }
+        }
+
+        protected async Task<IActionResult> UpdateComponent<T>(T component, bool isUpdated, string databaseName,
+            string[] characteristics) where T: Component<IFormFile>
+        {
+            string[] characteristicsBase = ["brand", "model", "country", "price", "description", "image", "amount", "power", "likes", "product_type"];
+            try
+            {
+                if (component.Price < 0)
+                {
+                    return BadRequest(new { error = "Price must not be less than 0" });
+                }
+
+                if (component.Amount < 0)
+                {
+                    return BadRequest(new { error = "Amount must not be less than 0" });
+                }
+
+                if (component.Power < 0 || component.Power > 10)
+                {
+                    return BadRequest(new { error = "Power must be between 0 and 10" });
+                }
+
+                if(component.Likes < 0)
+                {
+                    return BadRequest(new { error = "Likes must not be less than 0" });
+                }
+
+                await using var connection = new NpgsqlConnection(connectionString);
+                {
+                    connection.Open();
+                    string imagePath;
+                    string oldImagePath = connection.QueryFirstOrDefault<string>($"SELECT image FROM public.products WHERE " +
+                        $"product_id = {component.ProductId}");
+                    if (isUpdated)
+                    {
+                        BackupWriter.Delete(oldImagePath);
+                        imagePath = BackupWriter.Write(component.Image);
+                    }
+                    else
+                    {
+                        imagePath = oldImagePath;
+                    }
+
+                    PropertyInfo[] properties = typeof(T).GetProperties();
+                    Dictionary<string, object> baseComponentCharacteristics = new Dictionary<string, object>();
+                    Dictionary<string, object> componentCharacteristics = new Dictionary<string, object>();
+                    foreach (var property in properties)
+                    {
+                        bool isContain = characteristicsBase.Any(characteristic => SnakeCaseToPascalCase(characteristic) == property.Name);
+                        if (isContain)
+                        {
+                            if (property.Name == "Image")
+                            {
+                                baseComponentCharacteristics[property.Name] = imagePath;
+                            }
+                            else
+                            {
+                                baseComponentCharacteristics[property.Name] = property.GetValue(component);      
+                            }
+                        }
+                        else if(property.Name != "ProductId")
+                        {
+                            componentCharacteristics[property.Name] = property.GetValue(component);
+                        }
+                    }
+                    Dictionary<string, object> response = new Dictionary<string, object>();
+                    foreach (var elem in baseComponentCharacteristics)
+                    {
+                        response[elem.Key] = elem.Value;
+                    }
+                    foreach (var elem in componentCharacteristics)
+                    {
+                        response[elem.Key] = elem.Value;
+                    }
+                    connection.Execute($"UPDATE public.products SET {TransformCharacteristicsToSetString(characteristicsBase)} WHERE product_id = {component.ProductId}", response);
+                    connection.Execute($"UPDATE public.{databaseName} SET {TransformCharacteristicsToSetString(characteristics)} WHERE product_id = {component.ProductId}", response);
+                    logger.LogInformation("Component data updated in the database");
+                    
+                    return Ok(new { id = component.ProductId, data = response });
+                
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Failed to update component data in database. \nException: {ex}");
+                return StatusCode(500, new { error = ex.Message });
+            }
+        }
+
+        protected async Task<IActionResult> DeleteComponent(int id)
+        {
+            try
+            {
+                string filePath;
+
+                await using var connection = new NpgsqlConnection(connectionString);
+                {
+                    connection.Open();
+                    logger.LogInformation("Connection started");
+
+                    filePath = connection.QueryFirstOrDefault<string>($"SELECT image FROM public.products WHERE product_id = {id}");
+                    BackupWriter.Delete(filePath);
+
+                    connection.Execute($"DELETE FROM public.products WHERE product_id = {id}");
+
+                    logger.LogInformation("Component data deleted from the database");
+
+                    return Ok(new {id = id});
+                }
+
+            }
+            catch(Exception ex)
+            {
+                logger.LogError($"Failed to delete component data in database. \nException: {ex}");
+                return StatusCode(500, new { error = ex.Message });
             }
         }
 
@@ -126,40 +273,68 @@ namespace backend.Controllers
             return stringOfCharacteristics;
         }
 
-        private async Task<Dictionary<string, object>> JoinProductInfo(int productId, string databaseName, Component<string> productBase,
-            string[] productCharacteristics)
+        private static string TransformCharacteristicsToSetString(string[] characteristics)
+        {
+            string stringOfValues = "";
+            for (int i = 0; i < characteristics.Length; i++)
+            {
+                if (i != 0)
+                {
+                    stringOfValues += $", {characteristics[i]} = @{SnakeCaseToPascalCase(characteristics[i])}";   
+                }
+                else
+                {
+                    stringOfValues += $"{characteristics[i]} = @{SnakeCaseToPascalCase(characteristics[i])}"; 
+                }
+            }
+
+            return stringOfValues;
+        }
+        
+        private static string SnakeCaseToPascalCase(string snakeCase)
+        {
+            string[] words = snakeCase.Split('_');
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(words[i]);
+            }
+            return string.Concat(words);
+        }
+        
+        private async Task<Dictionary<string, object>> JoinComponentInfo<T>(int componentId, string databaseName, Component<string> componentBase,
+            string[] componentCharacteristics)
         {
             try
             {
                 await using var connection = new NpgsqlConnection(connectionString);
                 {
                     connection.Open();
-                    Dictionary<string, object> product = new Dictionary<string, object>();
-                    var productInfo =
-                        connection.Query<ComputerCaseInfo>($"SELECT {TransformCharacteristicsToString(productCharacteristics)} " +
-                                                           $"FROM public.{databaseName} WHERE product_id = {productId}");
+                    Dictionary<string, object> component = new Dictionary<string, object>();
+                    var componentInfo =
+                        connection.QueryFirstOrDefault<T>($"SELECT {TransformCharacteristicsToString(componentCharacteristics)} " +
+                                                           $"FROM public.{databaseName} WHERE product_id = {componentId}");
                     List<PropertyInfo> properties = new List<PropertyInfo>(typeof(Component<string>).GetProperties());
-                    for (int i = 0; i < typeof(ComputerCaseInfo).GetProperties().Length; i++)
+                    for (int i = 0; i < typeof(T).GetProperties().Length; i++)
                     {
                         bool isContains = properties.Any(property =>
-                            property.Name != typeof(ComputerCaseInfo).GetProperties()[i].Name);
+                            property.Name == typeof(T).GetProperties()[i].Name);
                         if (!isContains)
                         {
-                            properties.Add(typeof(ComputerCaseInfo).GetProperties()[i]);
+                            properties.Add(typeof(T).GetProperties()[i]);
                         }
                     }
                     foreach (var property in properties)
                     {
-                        if (property.GetValue(productBase) != null)
+                        if (typeof(Component<string>).GetProperties().Any(elem => elem.Name == property.Name))
                         {
-                            product[property.Name] = property.GetValue(productBase)!;
+                            component[property.Name] = property.GetValue(componentBase)!;
                         }
                         else
                         {
-                            product[property.Name] = property.GetValue(productInfo);
+                            component[property.Name] = property.GetValue(componentInfo);
                         }
                     }
-                    return product;
+                    return component;
                 }
             }
             catch(Exception ex)
--- backend/backend/Controllers/ComputerCaseController.cs
+++ backend/backend/Controllers/ComputerCaseController.cs
@@ -1,5 +1,6 @@
 ﻿using backend.Entities;
 using backend.Entities.CommentEntities;
+using backend.Entities.ComponentsInfo;
 using backend.Entities.User;
 using backend.UpdatedEntities;
 using backend.Utils;
@@ -39,183 +40,26 @@ namespace backend.Controllers
         [HttpGet("getAllComputerCases")]
         public async Task<IActionResult> GetAllComputerCases(int limit, int offset)
         {
-            return await GetAllComponents<ComputerCase<string>>(limit, offset, "computer_cases", ["material", "width", "height", "depth"]);
+            return await GetAllComponents<ComputerCaseInfo>(limit, offset, "computer_cases", ["material", "width", "height", "depth"]);
         }
 
         [HttpGet("getComputerCase/{id}")]
         public async Task<IActionResult> GetComputerCaseById (int id)
         {
-            try
-            {
-                
-
-                await using var connection = new NpgsqlConnection(connectionString);
-                {
-                    connection.Open();
-                    logger.LogInformation("Connection started");
-
-                    
-                    var computerCase = connection.QueryFirstOrDefault<ComputerCase<string>>("SELECT * FROM public.computer_case " +
-                        "WHERE Id = @Id",
-                        new { Id = id });
-
-                    if (computerCase != null)
-                    {
-                        logger.LogInformation($"Retrieved ComputerCase with Id {id} from the database");
-                        return Ok(new { id = id, computerCase});
-
-                    }
-                    else
-                    {
-                        logger.LogInformation($"ComputerCase with Id {id} not found in the database");
-                        return NotFound(new {error = $"Not found ComputerCase with {id}"});
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                logger.LogError($"Failed to retrieve ComputerCase data from the database. \nException {ex}");
-                return StatusCode(500, new { error = ex.Message });
-            }
+            return await getComponent<ComputerCaseInfo>(id, "computer_cases", ["material", "width", "height", "depth"]);
         }
 
         [HttpPut("updateComputerCase/{id}")]
-        public async Task<IActionResult> UpdateComputerCase(int id, [FromForm] UpdatedComputerCase updatedComputerCase)
+        public async Task<IActionResult> UpdateComputerCase(int id, [FromForm] ComputerCase<IFormFile> computerCase, [FromQuery] bool isUpdated)
         {
-            try
-            {
-
-                if (updatedComputerCase.Width < 10 || updatedComputerCase.Width > 100)
-                {
-                    return BadRequest(new { error = "Width must be between 10 and 100" });
-                }
-
-                if (updatedComputerCase.Height < 30 || updatedComputerCase.Height > 150)
-                {
-                    return BadRequest(new { error = "Height must be between 30 and 150" });
-                }
-
-                if (updatedComputerCase.Depth < 20 || updatedComputerCase.Depth > 100)
-                {
-                    return BadRequest(new { error = "Depth must be between 20 and 100" });
-                }
-
-                if (updatedComputerCase.Price < 0)
-                {
-                    return BadRequest(new { error = "Price must not be less than 0" });
-                }
-
-                if (updatedComputerCase.Amount < 0)
-                {
-                    return BadRequest(new { error = "Amount must not be less than 0" });
-                }
-
-                if (updatedComputerCase.Power < 0 || updatedComputerCase.Power > 10)
-                {
-                    return BadRequest(new { error = "Power must be between 0 and 10" });
-                }
-
-                if(updatedComputerCase.Likes < 0)
-                {
-                    return BadRequest(new { error = "Likes must not be less than 0" });
-                }
-
-
-                string imagePath = string.Empty;
-
-                await using var connection = new NpgsqlConnection(connectionString);
-                {
-
-                    string filePath = connection.QueryFirstOrDefault<string>("SELECT image FROM public.computer_case" +
-                        " WHERE Id = @id");
-
-                    if (updatedComputerCase.updated)
-                    {
-                        
-                        BackupWriter.Delete(filePath);
-                        imagePath = BackupWriter.Write(updatedComputerCase.Image);
-                    }
-                    else 
-                    {
-                        imagePath = filePath;
-                    }
-
-                    var data = new
-                    {
-                        id = id,
-                        brand = updatedComputerCase.Brand,
-                        model = updatedComputerCase.Model,
-                        country = updatedComputerCase.Country,
-                        material = updatedComputerCase.Material,
-                        width = updatedComputerCase.Width,
-                        height = updatedComputerCase.Height,
-                        depth = updatedComputerCase.Depth,
-                        price = updatedComputerCase.Price,
-                        description = updatedComputerCase.Description,
-                        image = imagePath,
-                        amount = updatedComputerCase.Amount,
-                        power = updatedComputerCase.Power,
-                        likes = updatedComputerCase.Likes,
-                    };
-
-                    connection.Open();
-                    logger.LogInformation("Connection started");
-
-                    connection.Execute("UPDATE public.computer_case SET Brand = @brand, Model = @model, Country = @country," +
-                        " Material = @material, Width = @width, Height = @height," +
-                        " Depth = @depth, Price = @price, Description = @description," +
-                        " Image = @image, Amount = @amount, Power = @power, Likes = @likes WHERE Id = @id", data);
-
-                    logger.LogInformation("ComputerCase data updated in the database");
-
-                    return Ok(new { id = id, data });
-
-                }
-
-                
-            }
-            catch (Exception ex)
-            {
-                logger.LogError($"Failed to update ComputerCase data in database. \nException: {ex}");
-                return StatusCode(500, new { error = ex.Message });
-            }
+            computerCase.ProductId = id;
+            return await UpdateComponent<ComputerCase<IFormFile>>(computerCase, isUpdated, "computer_cases", ["material", "width", "height", "depth"]);
         }
 
         [HttpDelete("deleteComputerCase/{id}")]
         public async Task<IActionResult> DeleteComputerCase(int id)
         {
-            try
-            {
-                string filePath;
-
-                await using var connection = new NpgsqlConnection(connectionString);
-                {
-                    connection.Open();
-                    logger.LogInformation("Connection started");
-
-                    filePath = connection.QueryFirstOrDefault<string>("SELECT image FROM public.computer_case WHERE Id = @id",
-                        new { Id = id });
-                    BackupWriter.Delete(filePath);
-
-                    connection.Execute("DELETE FROM public.computer_case WHERE Id = @id", new { id });
-
-                    connection.Execute("DELETE FROM public.like WHERE componentid = @id AND component = computer_case",
-                        new { id });
-
-                    connection.Execute("DELETE FROM public.comment WHERE component_id = @id AND component = computer_case",
-                        new { id });
-
-                    logger.LogInformation("ComputerCase data deleted from the database");
-
-                    return Ok(new {id = id});
-                }
-
-            }
-            catch(Exception ex)
-            {
-                logger.LogError($"Failed to delete ComputerCase data in database. \nException: {ex}");
-                return StatusCode(500, new { error = ex.Message });
-            }
+            return await DeleteComponent(id);
         }
 
         [HttpGet("search")]
